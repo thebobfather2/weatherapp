f27b0bf5f6a46a45ac64f96d9bd47c43b9f65f15
Bryan Pike <bryanandrewpike@gmail.com>
2023-03-27 19:32:55 -0400

    cleared errors in console

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 
 $(window).on('load', function () {
-    currentLocation();
-    checkLocalStorage();
+    // currentLocation();
+    // checkLocalStorage();
 });
 
 // API key
@@ -28,6 +28,13 @@ $("#search-button").on("click", function (event) {
     saveToLocalStorage(q);
 });
 
+function convertToC(fahrenheit) {
+    var fTempVal = fahrenheit;
+    var cTempVal = (fTempVal - 32) * (5 / 9);
+    var celcius = Math.round(cTempVal * 10) / 10;
+    return celcius;
+  }
+
 // get weather function
 function getWeather(q) {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + q + "&appid=" + APIKey;
@@ -42,5 +49,51 @@ function getWeather(q) {
         console.log(response)
         $(".cityList").empty()
         $("#days").empty()
+        var celcius = convertToC(response.main.temp);
+        var cityMain1 = $("<div col-12>").append($("<p><h2>" + response.name + ' (' + currentDate + ')' + "</h2><p>"));
+        var image = $('<img class="imgsize">').attr('src', 'http://openweathermap.org/img/w/' + response.weather[0].icon + '.png');        
+        var degreeMain = $('<p>').text('Temperature : ' + response.main.temp + ' °F (' + celcius + '°C)');
+        var humidityMain = $('<p>').text('Humidity : ' + response.main.humidity + '%');
+        var windMain = $('<p>').text('Wind Speed : ' + response.wind.speed + 'MPH');       
+        var uvIndexcoord = '&lat=' + response.coord.lat + '&lon=' + response.coord.lon;
+        var cityId = response.id;
+
+        // displayUvindex(uvIndexcoord);
+        // displayForecast(cityId);
+
+        cityMain1.append(image).append(degreeMain).append(humidityMain).append(windMain);
+        $('#cityList').empty();
+        $('#cityList').append(cityMain1);
     });
+}
+
+// set up storage of city search
+function checkLocalStorage() {
+    var storedData = localStorage.getItem('queries');
+    var dataArray = [];
+    if (!storedData) {
+        console.log("no data stored");
+    } else {
+        storedData.trim();
+        dataArray = storedData.split(',');
+        for (var i = 0; i < dataArray.length; i++) {
+            createRecentSearchBtn(dataArray[i]);
+        }
+    }
+};
+
+function saveToLocalStorage(q) {
+    var data = localStorage.getItem('queries');
+    if (data) {
+        console.log(data, q)
+
+    } else {
+        data = q;
+        localStorage.setItem('queries', data);
+    }
+    if (data.indexOf(q) === -1) {
+        data = data + ',' + q;
+        localStorage.setItem('queries', data);
+        createRecentSearchBtn(q);
+    }
 }
